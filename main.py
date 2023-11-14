467d9a1194f2ae8792445d82a719f6e1c606dfe3
Mohan Kumar <smhnkmr@gmail.com>
2023-11-14 14:22:06 +0530

    color detection using opencv

---
+++ main.py
@@ -0,0 +1,33 @@
+import cv2
+from PIL import Image
+from util import get_limits
+
+yellow = [0, 255, 0] # BGR
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = cap.read()
+
+    hsvImage = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+    lowerLimit, upperLimit = get_limits(color=yellow)
+
+    mask = cv2.inRange(hsvImage, lowerLimit, upperLimit)
+
+    mask_pil = Image.fromarray(mask)
+
+    bbox = mask_pil.getbbox()
+
+    if bbox is not None:
+        x1, y1, x2, y2 = bbox
+
+        frame = cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
+
+    cv2.imshow('frame', frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
